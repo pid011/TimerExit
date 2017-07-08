03ee97667d9bfe3e369b29f848dcd517e8e1f9f8
Sepi <deet1022@gmail.com>
2017-07-09 00:39:55 +0900

    Update solution

--- src/Main.c
+++ src/Main.c
@@ -3,6 +3,50 @@
 #include <Windows.h>
 #include <conio.h>
 
+int getTime() {
+	int hour, min, sec;
+
+	printf("[Console] 시간 입력. ex)\'0\' 시간 \'30\' 분뒤면 \'0\' 이라고 입력.\n");
+	printf("[Console] 입력:");
+	scanf_s("%d", &hour);
+
+	printf("[Console] 분 입력. ex)\'0\' 시간 \'30\' 분뒤면 \'30\' 이라고 입력.\n");
+	printf("[Console] 입력:");
+	scanf_s("%d", &min);
+	puts("[Console] \n...\n");
+	hour *= 3600;
+	min *= 60;
+	return sec = hour + min;
+}
+
+int setCommand(int choice) {
+	int sec = 0;
+	char cmd[100];
+
+	if (choice != 1 && choice != 2)
+	{
+		puts("[Console] error : 해당 명령어는 없는 명령어 입니다.");
+		puts("[Console] 아무 키나 누를 시 종료됩니다.");
+		_getch();
+		exit(0);
+	}
+	if (choice == 1)
+	{
+		sec = getTime();
+		sprintf_s(cmd, sizeof(cmd), "shutdown /s /t %d", sec);
+		system(cmd);
+		printf("[Console] \'종료\'를 예약하셨습니다.\n");
+	}
+	else if (choice == 2)
+	{
+		sec = getTime();
+		sprintf_s(cmd, sizeof(cmd), "shutdown /r /t %d", sec);
+		system(cmd);
+		printf("[Console] \'다시시작\'을 예약하셨습니다.\n");
+	}
+	return sec;
+}
+
 int main(void) {
 	int choices = 0;
 	int sec = 0;
@@ -40,48 +84,4 @@ int main(void) {
 
 	_getch();
 	return 0;
-}
-
-int setCommand(int choice) {
-	int sec = 0;
-	char cmd[100];
-
-	if (choice != 1 && choice !=2)
-	{
-		puts("[Console] error : 해당 명령어는 없는 명령어 입니다.");
-		puts("[Console] 아무 키나 누를 시 종료됩니다.");
-		_getch();
-		exit(0);
-	}
-	if (choice == 1)
-	{
-		sec = getTime();
-		sprintf_s(cmd, sizeof(cmd), "shutdown /s /t %d", sec);
-		system(cmd);
-		printf("[Console] \'종료\'를 예약하셨습니다.\n");
-	}
-	else if (choice == 2)
-	{
-		sec = getTime();
-		sprintf_s(cmd, sizeof(cmd), "shutdown /r /t %d", sec);
-		system(cmd);
-		printf("[Console] \'다시시작\'을 예약하셨습니다.\n");
-	}
-	return sec;
-}
-
-int getTime() {
-	int hour, min, sec;
-
-	printf("[Console] 시간 입력. ex)\'0\' 시간 \'30\' 분뒤면 \'0\' 이라고 입력.\n");
-	printf("[Console] 입력:");
-	scanf_s("%d", &hour);
-
-	printf("[Console] 분 입력. ex)\'0\' 시간 \'30\' 분뒤면 \'30\' 이라고 입력.\n");
-	printf("[Console] 입력:");
-	scanf_s("%d", &min);
-	puts("[Console] \n...\n");
-	hour *= 3600;
-	min *= 60;
-	return sec = hour + min;
 }
